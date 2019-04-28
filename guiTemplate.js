f9e7d04f2303ad35682d0e47bd18f3fa738cabb8
Cyborg_Ean <ean.director@gmail.com>
2019-04-28 16:45:10 -0700

    committing to get syntax highlighting

---
+++ guiTemplate.js
@@ -0,0 +1,7 @@
+var mainComp = app.project.activeItem;
+var mainLayer = mainComp.layer(1);
+
+var guiTitle = "Gui Title Goes Here";
+var mainGUI = new Window("palette", guiTitle, undefined);
+
+var group1Data;
