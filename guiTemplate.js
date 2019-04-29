77d2dcbf2b2ca0d6cfc27bb4607b1c224f3a5c0b
Cyborg_Ean <ean.director@gmail.com>
2019-04-28 17:17:32 -0700

    Update guiTemplate.js
    added boiler plate code, preparing to test it and handle bug fixes, script is currently not stable.

--- guiTemplate.js
+++ guiTemplate.js
@@ -4,4 +4,26 @@ var mainLayer = mainComp.layer(1);
 var guiTitle = "Gui Title Goes Here";
 var mainGUI = new Window("palette", guiTitle, undefined);
 
-var group1Data;
+var group1 = mainGui.add("group", undefined, "group1");
+group1.orientation = "column";
+
+var g1Description = group1.add("statictext", undefined, "This is where you add your program description!");
+var g1Title       = group1.add("edittext", undefined, "Title");
+var g1TitleDuration = group1.add("edittext", undefined, "Duration");
+var textInput = group1.add("edittext", undefined, "Input your text here");
+
+var group2 = mainGui.add("group", undefined, "group2");
+group2.orientation = "row";
+
+var btn1 = group2.add("button", undefined, "start");
+var btn2 = group1.add("button", undefined, "cancel");
+
+btn1.onClick = function(arg){
+    writeLn("Btn1 has been clicked!");
+    writeLn("Btn1 arg is: " + arg);
+};
+
+btn2.onClick = function(){
+    writeLn("Btn2 has been clicked!");
+    mainGui.close();
+}
